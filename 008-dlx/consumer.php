6b2d060be6173930491e819ef8f87ad6ef46cd5c
Vranac Srdjan <vranac@code4hire.com>
2016-08-31 10:22:58 +0200

    Dead Poets... erm, Deat Letter Exchange.

---
+++ 008-dlx/consumer.php
@@ -0,0 +1,46 @@
+<?php
+
+use PhpAmqpLib\Message\AMQPMessage;
+
+/**
+ * @var \PhpAmqpLib\Channel\AMQPChannel $channel
+ */
+$channel = require __DIR__ . '/../channel.php';
+
+$handler = function(AMQPMessage $message) use ($channel) {
+    print $message->body . PHP_EOL;
+    // Remove RPC stuff
+    $channel->basic_ack($message->delivery_info['delivery_tag']);
+};
+
+// Convert to temporary queue
+$q = $channel->queue_declare(
+    '',
+    false,
+    false,
+    false,
+    true // auto-delete to true
+);
+$queue_name = $q[0];
+
+$channel->exchange_declare(
+    'test_dlx_exchange', // exchange name changes to the DLX exchange
+    'fanout' // simply set to fanout
+);
+
+// Simplfy (no BK) and also change exchange name
+$channel->queue_bind($queue_name, 'test_dlx_exchange');
+
+$channel->basic_consume(
+    $queue_name,
+    'worker' . getmypid(),
+    false,
+    false,
+    false,
+    false,
+    $handler
+);
+
+while (count($channel->callbacks)) {
+    $channel->wait();
+}
---
+++ 008-dlx/producer.php
@@ -0,0 +1,26 @@
+<?php
+
+// BASED OFF: t7-ttl/3-per-queue-message.php
+
+use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Wire\AMQPTable;
+
+/**
+ * @var \PhpAmqpLib\Channel\AMQPChannel $channel
+ */
+$channel = require __DIR__ . '/../channel.php';
+
+$metadata = new AMQPTable([
+    'x-message-ttl' => 10000,
+    'x-dead-letter-exchange' => 'test_dlx_exchange', // Add name of DLX exchange
+    // 'x-dead-letter-routing-key' => 'blah'
+]);
+$channel->queue_declare('test_dlx_queue', false, false, false, false, false, $metadata);
+
+$message = new AMQPMessage('Hello, Web Summer Camp 16 workshop!!');
+
+$channel->basic_publish(
+    $message,
+    '',
+    'test_dlx_queue' // queue name change
+);
