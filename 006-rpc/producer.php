fde14cf10e5cb2d031afb2d487ee687d1eb694de
Vranac Srdjan <vranac@code4hire.com>
2016-08-31 10:11:32 +0200

    Remote Procedure Calls.

---
+++ 006-rpc/producer.php
@@ -0,0 +1,42 @@
+<?php
+
+use PhpAmqpLib\Message\AMQPMessage;
+
+/**
+ * @var \PhpAmqpLib\Channel\AMQPChannel $channel
+ */
+$channel = require __DIR__ . '/../channel.php';
+
+$channel->exchange_declare(
+    'test_topic_exchange',
+    'topic'
+);
+
+// Correlation ID is a unique request/response pair identifier
+$correlation_id = microtime(true) . '_' . getmypid();
+
+// Listen to a temporary response queue
+$q = $channel->queue_declare('', false, false, true, true);
+$callback_queue = $q[0];
+$channel->basic_consume($callback_queue, '', false, false, false, false, function (AMQPMessage $message) use ($correlation_id) {
+    if ($message->get('correlation_id') === $correlation_id) {
+        print "Response was: " . $message->body . PHP_EOL;
+    }
+});
+
+// We now add some metadata in here
+$metadata = [
+    'correlation_id' => $correlation_id,
+    'reply_to' => $callback_queue,
+];
+$message = new AMQPMessage('Hello, Web Summer Camp 16 workshop!!', $metadata);
+
+$routing_key = $argv[1];
+$channel->basic_publish(
+    $message,
+    'test_topic_exchange',
+    $routing_key
+);
+
+// We need to wait for a response (once)
+$channel->wait();
