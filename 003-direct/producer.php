5351741ec116a4a9292c6c242315bb3d6e2aba39
Vranac Srdjan <vranac@code4hire.com>
2016-08-31 09:49:38 +0200

    Direct.

---
+++ 003-direct/producer.php
@@ -0,0 +1,24 @@
+<?php
+
+use PhpAmqpLib\Message\AMQPMessage;
+
+/**
+ * @var \PhpAmqpLib\Channel\AMQPChannel $channel
+ */
+$channel = require __DIR__ . '/../channel.php';
+
+// declare the exchange (type direct)
+$channel->exchange_declare(
+    'test_direct_exchange',
+    'direct'
+);
+
+$message = new AMQPMessage('Hello, Web Summer Camp 16 workshop!!');
+
+// Fetch routing key from CLI param
+$routing_key = $argv[1];
+$channel->basic_publish(
+    $message,
+    'test_direct_exchange',
+    $routing_key // queue name is back!? This is the ROUTING KEY
+);
