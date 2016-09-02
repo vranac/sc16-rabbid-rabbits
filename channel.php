d330ed1b0fef22176203ec4eda592bf60750f212
Vranac Srdjan <vranac@code4hire.com>
2016-09-02 09:22:08 +0200

    Add the shared code.

---
+++ channel.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'worker', 'worker');
+
+return $connection->channel();
